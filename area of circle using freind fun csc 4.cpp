0703232aa06799b9c615d90ba77e43634922fada
Bijaylakshmi Kumari <112059431+Bijaylakshmi@users.noreply.github.com>
2022-09-01 14:55:08 +0530

    area of a circle

---
+++ area of circle using freind fun csc 4.cpp
@@ -0,0 +1,39 @@
+//finding out ara of circle  using freind fun
+#include<iostream>
+using namespace std;
+class area
+{
+int r;
+float pi;
+public:
+void getdata()
+{
+cout<<"enter the value of radius=";
+cin>>r;
+}
+friend void aoc(area ob);
+};
+void aoc (area ob)
+{
+ob.pi=3.14;
+float ans;
+ans=ob.pi*ob.r*ob.r;
+cout<<"area is="<<ans;
+}
+main()
+{
+class area obj;
+obj.getdata();
+aoc(obj);
+}
+
+
+
+
+
+
+
+
+
+
+
